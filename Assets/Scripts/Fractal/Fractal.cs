e82e368ac520ef62b1cdef235e9fd7aa7dd5e15b
Tyler Funk <tcfunk25@gmail.com>
2014-07-30 18:49:06 -0500

    Added fun twists and colors and other randomness

--- Assets/Scripts/Fractal/Fractal.cs
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -3,18 +3,29 @@ using System.Collections;
 
 public class Fractal : MonoBehaviour {
 
-	public Mesh mesh;
+	public Mesh[] meshes;
 	public Material material;
 	
-	public int maxDepth = 5;
-	public float childScale = 0.5f;
-	public float spawnRate = 0.3f;
+	[Range(0,6)]
+	public int maxDepth;
+	
+	[Range(0,1)]
+	public float childScale;
+	
+	[Range(0,1)]
+	public float spawnChance;
+	
+	[Range(0, 180)]
+	public float maxTwist;
+	
+	public float maxRotationSpeed;
 	
 	private int depth = 0;
-	private float degrees = 0.0f;
 	private bool isRoot = true;
+	private float rotationSpeed;
+	private Material[,] materials;
 	
-	private Vector3[] childDirections = {
+	private static Vector3[] childDirections = {
 		Vector3.up,
 		Vector3.left,
 		Vector3.right,
@@ -23,7 +34,7 @@ public class Fractal : MonoBehaviour {
 		Vector3.down  // For root object only
 	};
 	
-	private Quaternion[] childOrientations = {
+	private static Quaternion[] childOrientations = {
 		Quaternion.identity,
 		Quaternion.Euler (0f, 0f, 90f),
 		Quaternion.Euler (0f, 0f, -90f),
@@ -31,11 +42,26 @@ public class Fractal : MonoBehaviour {
 		Quaternion.Euler (-90f, 0f, 0f),
 		Quaternion.Euler (180f, 0f, 0f)  // For root object only
 	};
+	
+	private static Color[] colors = {
+		Color.blue,
+		Color.green,
+		Color.yellow,
+		Color.magenta,
+		Color.cyan,
+		Color.red
+	};
 
 	// Use this for initialization
 	void Start () {
-		gameObject.AddComponent<MeshFilter>().mesh = mesh;
-		gameObject.AddComponent<MeshRenderer>().material = material;	
+		if (materials == null) {
+			InitializeMaterials();
+		}
+		rotationSpeed = Random.Range (-maxRotationSpeed, maxRotationSpeed);
+		transform.Rotate(Random.Range (-maxTwist, maxTwist), 0f, 0f);
+		gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range (0, meshes.Length)];
+		gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range (0, colors.Length)];	
+//		renderer.material.color = Color.Lerp(Color.white, colors[Random.Range(0, colors.Length)], (float) depth / maxDepth);
 		if (depth < maxDepth) {
 			StartCoroutine(SpawnChildren());
 		}
@@ -44,18 +70,24 @@ public class Fractal : MonoBehaviour {
 	public IEnumerator SpawnChildren() {
 		int numToSpawn = isRoot ? childDirections.Length : childDirections.Length - 1;
 		for (int i = 0; i < numToSpawn; i++) {
-			yield return new WaitForSeconds(Random.Range (0.1f, spawnRate));
-			new GameObject("Child").AddComponent<Fractal>().Initialize(this, i);
+			if (Random.value < spawnChance) {
+				if (!isRoot) { yield return new WaitForSeconds(Random.Range (0.1f, 0.5f)); }
+				new GameObject("Child").AddComponent<Fractal>().Initialize(this, i);
+			}
 		}
 	}
 	
 	public void Initialize(Fractal parent, int childIndex) {
-		mesh = parent.mesh;
+		meshes = parent.meshes;
 		material = parent.material;
 		maxDepth = parent.maxDepth;
 		depth = parent.depth + 1;
 		childScale = parent.childScale;
-		isRoot = false;
+		spawnChance = parent.spawnChance;
+		maxRotationSpeed = parent.maxRotationSpeed;
+		maxTwist = parent.maxTwist;
+		
+		isRoot = false; // or else we'll get a lot of extra thingers
 		
 		transform.parent = parent.transform;
 		transform.localScale = Vector3.one * childScale;
@@ -63,11 +95,20 @@ public class Fractal : MonoBehaviour {
 		transform.localRotation = childOrientations[childIndex];
 	}
 	
+	private void InitializeMaterials() {
+		materials = new Material[maxDepth + 1, colors.Length];
+		for (int i = 0; i <= maxDepth; i++) {
+			float t = i / (maxDepth - 1f);
+			t *= t;
+			for (int j = 0; j < colors.Length; j++) {
+				materials[i, j] = new Material(material);
+				materials[i, j].color = Color.Lerp (Color.white, colors[j], t);				
+			}
+		}
+	}
+	
 	// Update is called once per frame
 	void Update () {
-		if (isRoot) {
-			degrees = degrees + 1.0f % 360.0f;
-			transform.localRotation = Quaternion.AngleAxis(degrees, Vector3.up);
-		}
+		transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
 	}
 }
